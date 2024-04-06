d29567df90e9ecfa5ee1ab939aaf36a180d5413b
gaurav <gauravpathak9058@gmail.com>
2024-04-06 12:40:51 +0530

    jk

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import styles from "./writePage.module.css";
 import { useEffect, useState } from "react";
-import "react-quill/dist/quill.bubble.css";
+// import "react-quill/dist/quill.bubble.css";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 // import {
@@ -13,7 +13,7 @@ import { useSession } from "next-auth/react";
 //   getDownloadURL,
 // } from "firebase/storage";
 // import { app } from "@/utils/firebase";
-import ReactQuill from "react-quill";
+// import ReactQuill from "react-quill";
 
 const WritePage = () => {
   // const { status } = useSession();
